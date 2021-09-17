9c7c41a810d4ebc6fe7d7105bdb218bd105fb3ff
Suchi Undevia <suchi.undevia@gmail.com>
2021-09-17 23:12:17 +1200

    Section 4 complete

---
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+
+import './ExpenseForm.css'
+
+const ExpenseForm = (props) => {
+    // single states for each input. 
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    // use state to track all three inputs bunched together in an object
+    // const [userInput, setUserInput] = useState(
+    //     {
+    //         enteredTitle: '',
+    //         enteredAmount: '',
+    //         enteredDate: new Date(enteredDate),
+    //     }
+    // );
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // })
+        // use a function inside a function to set the values taken from user input especially when you depend on previous state.
+        // this has to do with react scheduling and may cause problems.
+        // using two functions you ensure react gives you the latest version of the state snapshot therefore safer approach.
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredTitle: event.target.value };
+        // });
+    };
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    };
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate),
+        };
+        props.onSaveExpenseData(expenseData);
+        // two way binding - gather information and set it back to null.
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
+
+    return <form onSubmit={submitHandler}>
+        <div className="new-expense__controls">
+            <div className="new-expense__control">
+                <label>Title</label>
+                <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+            </div>
+            <div className="new-expense__control">
+                <label>Amount</label>
+                <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
+            </div>
+            <div className="new-expense__control">
+                <label>Date</label>
+                <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler} />
+            </div>
+        </div>
+        <div className="new-expense__acitons">
+            <button type="submit">Add Expense</button>
+        </div>
+    </form>;
+};
+
+export default ExpenseForm;
