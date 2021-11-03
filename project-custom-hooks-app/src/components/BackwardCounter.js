093decee37e09a43dfafafa0e250cec24db107ae
Suchi Undevia <suchi.undevia@gmail.com>
2021-11-03 22:31:13 +1300

    Section 15 complete

---
+++ project-custom-hooks-app/src/components/BackwardCounter.js
@@ -0,0 +1,10 @@
+import Card from "./Card";
+import useCounter from "../hooks/useCounter";
+
+const BackwardCounter = () => {
+  const counter = useCounter(false);
+
+  return <Card>{counter}</Card>;
+};
+
+export default BackwardCounter;
